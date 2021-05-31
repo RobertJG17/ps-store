a68ebfaaa88f42f7f3078a8f7c5feec772765322
Cory Ortega <coryortega@Corys-MBP.home>
2021-05-30 19:18:27 -0700

    cleaned file

--- top_artists.py
+++ top_artists.py
@@ -1,12 +1,22 @@
-import config
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
-scope = 'user-top-read'
+sp = spotipy.Spotify(auth="BQARNVdE3z-4k6WvEUYPihU19NWMgcpZCHtY5HwUZ_Gazz8PWQB-fGs0YHsbM-bjqh9_GjkwJiULxn2k34MFRY5j_YNUJGG4Z8OrBth_T1F0IuSxyTq5PIy4dHD-2fbTQ4EMaoXl4oq1SAhs_Iz2mBCWtClkIYRt9PV-8t-4x434HNhUVVUV-50kuvVrSaqywIuPkdtkBcHh07COVgb4ajZBuI0GzsdIH4GHPmzjBjzn2eQGy8-MBvRp1TBPi-vpoIlxTR41l5IhDVmQlOEfEAuqT2CubxyrIvZ0Ikks82f1")
+
+
 ranges = ['short_term', 'medium_term', 'long_term']
 
-sp = config.sp
-results = sp.current_user_top_artists(time_range=ranges[2], limit=50)
+results = sp.current_user_top_artists(time_range=ranges[1], limit=50)
 
 
-print(results)
-# for result in results:
-#     print(result)
+for result in results["items"]:
+    print("name: ", result["name"], "uri: ", result["uri"], "\n")
+
+# results = spotify.artist_albums(birdy_uri, album_type='album')
+# albums = results['items']
+# while results['next']:
+#     results = spotify.next(results)
+#     albums.extend(results['items'])
+
+# for album in albums:
+#     print(album['name'])
