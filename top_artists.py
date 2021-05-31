08bbe7dfb25603e3b9ef7d6e54a7f3670fb11e11
RobertJG17 <robertjguerra@yahoo.com>
2021-05-30 19:38:18 -0700

    Started Integration with Jupyter for Data Analysis Integration and Visualization

--- top_artists.py
+++ top_artists.py
@@ -1,4 +1,6 @@
 import spotipy
+import numpy as np
+import pandas as pd
 from spotipy.oauth2 import SpotifyClientCredentials
 
 sp = spotipy.Spotify(auth="BQARNVdE3z-4k6WvEUYPihU19NWMgcpZCHtY5HwUZ_Gazz8PWQB-fGs0YHsbM-bjqh9_GjkwJiULxn2k34MFRY5j_YNUJGG4Z8OrBth_T1F0IuSxyTq5PIy4dHD-2fbTQ4EMaoXl4oq1SAhs_Iz2mBCWtClkIYRt9PV-8t-4x434HNhUVVUV-50kuvVrSaqywIuPkdtkBcHh07COVgb4ajZBuI0GzsdIH4GHPmzjBjzn2eQGy8-MBvRp1TBPi-vpoIlxTR41l5IhDVmQlOEfEAuqT2CubxyrIvZ0Ikks82f1")
@@ -7,10 +9,6 @@ sp = spotipy.Spotify(auth="BQARNVdE3z-4k6WvEUYPihU19NWMgcpZCHtY5HwUZ_Gazz8PWQB-f
 ranges = ['short_term', 'medium_term', 'long_term']
 
 results = sp.current_user_top_artists(time_range=ranges[1], limit=50)
-
-
-for result in results["items"]:
-    print("name: ", result["name"], "uri: ", result["uri"], "\n")
 
 # results = spotify.artist_albums(birdy_uri, album_type='album')
 # albums = results['items']
