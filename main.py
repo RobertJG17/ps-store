17f756ed7ba2d2c012da8c44810086a9ac565991
RobertJG17 <robertjguerra@yahoo.com>
2021-05-31 16:45:37 -0700

    Rendered SVG File to Flas Endpoint

--- main.py
+++ main.py
@@ -1,25 +1,13 @@
-from flask import Flask
-from bs4 import BeautifulSoup
-from requests import request
-
+from flask import Flask, render_template, Markup
 
 app = Flask(__name__)
-
-res = request(url="https://store.playstation.com/en-us/category/35027334-375e-423b-b500-0d4d85eff784/1",
-              method="GET")
-
-soup = BeautifulSoup(res.text, parser="lxml", features="lxml")
-print(len(soup))
-print(soup)
-cells = soup.find_all("div", attrs={"class": "ems-sdk-product-tile"})
-
-for cell in cells:
-    print(cell)
 
 
 @app.route('/')
 def index():
-    return 'yeet'
+    svg = open("radar.svg").read()
+
+    return svg
 
 
 if __name__ == '__main__':
